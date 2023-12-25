2f91576c72a4ecb1ee0e39ae5e23d40e5a5eabc8
Clark <mcevoyclark@gmail.com>
2023-12-25 17:09:04 +0100

    fist init

---
+++ lua/clarkagen/init.lua
@@ -0,0 +1,2 @@
+require("clarkagen.remap")
+require("clarkagen.set")
---
+++ lua/clarkagen/set.lua
@@ -0,0 +1,23 @@
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
