b542539049bc3977584f405b204d10768e3c1403
Clark <mcevoyclark@gmail.com>
2023-12-26 13:19:52 +0100

    add undo tree settings

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -179,6 +179,11 @@ _G.packer_plugins = {
     path = "/home/clark/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
     url = "https://github.com/folke/tokyonight.nvim"
   },
+  undotree = {
+    loaded = true,
+    path = "/home/clark/.local/share/nvim/site/pack/packer/start/undotree",
+    url = "https://github.com/mbbill/undotree"
+  },
   ["zen-mode.nvim"] = {
     loaded = true,
     path = "/home/clark/.local/share/nvim/site/pack/packer/start/zen-mode.nvim",
