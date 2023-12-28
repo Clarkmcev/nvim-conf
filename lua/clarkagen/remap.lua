576265c1ed09ed96d6f37dc06a900bfeb384761a
Clark <mcevoyclark@gmail.com>
2023-12-28 19:51:51 +0100

    feat: nvim telescope keymaps config

--- lua/clarkagen/remap.lua
+++ lua/clarkagen/remap.lua
@@ -53,7 +53,13 @@ vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
 
+-- user configs
 vim.keymap.set("n", "ui", function() vim.cmd(":u") end)
 
 -- telescope config
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]ilter' })
+vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch [W]ord' })
+vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+
